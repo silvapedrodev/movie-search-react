428f8721bb28cfcc25f43875498145dd3e1195bf
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-02-27 10:35:29 -0300

    feat: improve user name input and fix styles

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,9 @@
-import { redirect } from 'next/navigation'
-
-import { LogoutButton } from '@/components/logout-button'
-import { getUserSession } from '@/lib/auth'
 import { DashboardContent } from '@/components/dashboard/dashboard-content'
 
-export default async function ProfilePage() {
-  const { username, isLoggedIn  } = await getUserSession()
-  if (!isLoggedIn || !username) redirect('/auth/login')
-
+export default function ProfilePage() {
   return (
     <main>
-      <DashboardContent user={username}/>
+      <DashboardContent />
     </main>
   )
 }
--- src/components/dashboard/dashboard-content.tsx
+++ src/components/dashboard/dashboard-content.tsx
@@ -1,30 +1,30 @@
-import { Filter, Pencil } from "lucide-react"
-import { Button } from "../ui/button"
-import { LogoutButton } from "../logout-button"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs"
-import { DashboardSection } from "./dashboard-section"
+"use client"
 
-type Props = {
-  user: string
-}
+import { Pencil } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { LogoutButton } from "@/components/logout-button"
+import { DashboardSection } from "@/components/dashboard/dashboard-section"
+import { useAuth } from "@/context/auth-context"
 
-export const DashboardContent = ({ user }: Props) => {
+export const DashboardContent = () => {
+  const { username } = useAuth()
+
   return (
     <div className="w-screen h-screen mx-auto px-4 sm:px-6 lg:px-20 pt-24">
       <div className="flex flex-col md:flex-row md:items-end justify-between mb-8 gap-4  py-4">
         <div>
-          <h1 className="text-2xl md:text-3xl font-bold">Hello, {user}</h1>
+          <h1 className="text-2xl md:text-3xl font-bold">Hello, {username}</h1>
           <p>Welcome back! Check your lists, track what you've seen, or update your settings.</p>
         </div>
         <div className="flex gap-2">
-          <Button variant="secondary" className="py-6 bg-purple-850 text-white rounded-lg hover:cursor-pointer hover:bg-purple-850/50">
+          <Button variant="secondary" className="py-6 bg-slate-800 text-white rounded-lg hover:cursor-pointer hover:bg-purple-850">
             <Pencil size={18} />
             Edit Profile
           </Button>
           <LogoutButton />
         </div>
       </div>
-        <DashboardSection />
+      <DashboardSection />
     </div>
 
   )
