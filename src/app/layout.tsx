83f6cd7fb6f1931ae62cff2771b8b154ab7f5504
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-03-03 14:42:48 -0300

    chore: remove unused functions and imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "Movie Search",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
