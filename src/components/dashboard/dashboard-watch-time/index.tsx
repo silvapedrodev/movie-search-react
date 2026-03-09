ac27a749327f8eef866a49842a54063a550e0593
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-03-09 17:12:11 -0300

    style(dashboard): improve desktop grid layout

--- src/components/dashboard/dashboard-watch-time/index.tsx
+++ src/components/dashboard/dashboard-watch-time/index.tsx
@@ -2,17 +2,26 @@ import { DailyGoal } from "@/components/dashboard/dashboard-watch-time/daily-goa
 import { LogActivity } from "@/components/dashboard/dashboard-watch-time/log-activity"
 import { DailyProgress } from "@/components/dashboard/dashboard-watch-time/daily-progress"
 import { WeeklyCalendar } from "@/components/dashboard/dashboard-watch-time/weekly-calendar"
-import { WatchTimeChart } from "./charts/watch-time-chart"
+import { WatchTimeChart } from "@/components/dashboard/dashboard-watch-time/charts/watch-time-chart"
 
 export const Index = () => {
   return (
     <div className="py-7">
       <div className="flex flex-col gap-6">
+
         <DailyProgress />
-        <LogActivity />
-        <WeeklyCalendar />
-        <DailyGoal />
-        <WatchTimeChart />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <LogActivity />
+          <WeeklyCalendar />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
+
+          <DailyGoal />
+          <WatchTimeChart />
+        </div>
+
       </div>
     </div>
   )
